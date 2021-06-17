8e2d31c25c6eb9e519e61c21e66bc308da395d9e
sasank <sasank2102@gmail.com>
2021-06-17 10:19:36 +0530

    Changes to be committed:
    	added add3() to add.c :
    		modified:   CCode/add.c
    
    	added HW2() to helloWorld.c:
    		modified:   CCode/helloWorld.c

--- CCode/add.c
+++ CCode/add.c
@@ -4,6 +4,10 @@ int add(int a,int b) {
 	return a+b;
 }
 
+int add3(int a,int b,int c){
+	return a+b+c;
+}
+
 
 int main(){
 
--- CCode/helloWorld.c
+++ CCode/helloWorld.c
@@ -1,5 +1,11 @@
 #include<stdio.h> 
 
+void hw2(){
+
+	printf("HW2");
+}
+
+
 int main(){
 
 	printf("hello world");
